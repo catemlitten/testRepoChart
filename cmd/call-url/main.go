18d4fc3e054d3f9057c91880f837af302f24796d
Cat Litten <catlitten@honeycomb.io>
2024-09-17 16:44:25 -0700

    heaven help me understand go

--- cmd/call-url/main.go
+++ cmd/call-url/main.go
@@ -1,33 +1,37 @@
 package main
 
 import (
-	"context"
+	"bytes"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-	"path"
-	"path/filepath"
-	"http"
-
-	"gopkg.in/yaml.v3"
+	"net/http"
 )
 
-
-func notify_swarmia(secret) nil {
+// can i notate this as secret?
+func notify_swarmia(secret *string) error {
 	payload_prep := fmt.Sprintf(`{
         "version": %s, 
         "appName": "hound-fake",
         "environment": %s,
         "repositoryFullName": "honeycombio/fakerepo",
-        "commitSha": %s}`, secret, secret, sceret)
+        "commitSha": %s}`, secret, secret, secret)
 	payload := []byte(payload_prep)
 	req, err := http.NewRequest("GET", "https://hook.swarmia.com/deployments", bytes.NewBuffer(payload))
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Add("Authorization", secret) // I need to set this in the github secrets and pass it here somehow
+	req.Header.Add("Authorization", *secret) // I need to set this in the github secrets and pass it here somehow
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+var l = log.New(os.Stderr, "", 0)
 
 func main() {
-	err := notify_swarmia()
+	secret := flag.String("secret", "", "The secret to test")
+	err := notify_swarmia(secret)
 
 	if err != nil {
 		l.Fatalln(err)
--- go.mod
+++ go.mod
@@ -1,5 +1,10 @@
-module cmd/promote-build/main.go
+module github/catemlitten/testRepoChart
 
 go 1.22.3
 
 require gopkg.in/yaml.v3 v3.0.1
+
+require (
+	github.com/kr/pretty v0.2.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
