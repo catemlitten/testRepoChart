45627e844dba5456a997672091b6d0e1f4d9bef9
Cat Litten <catlitten@honeycomb.io>
2024-09-18 10:33:14 -0700

    see how this works"

--- cmd/call-url/main.go
+++ cmd/call-url/main.go
@@ -21,8 +21,21 @@ func notify_swarmia(secret *string) error {
 	req, err := http.NewRequest("GET", "https://hook.swarmia.com/deployments", bytes.NewBuffer(payload))
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Add("Authorization", *secret) // I need to set this in the github secrets and pass it here somehow
+	resp, err := httpClient.Do(req)
 	if err != nil {
 		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusUnauthorized {
+		Println("Unauthorized")
+		return nil
+	}
+	body, _ := io.ReadAll(resp.Body)
+	if resp.StatusCode != http.StatusOK {
+		Println(resp.StatusCode)
+		Println(string(body))
+		Println(err.Error())
+		return nil
 	}
 	return nil
 }
